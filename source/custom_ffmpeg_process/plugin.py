7d03ff26537f79b717b2e6476c0e74c6c41545a2
Josh.5 <jsunnex@gmail.com>
2021-03-27 15:13:37 +1300

    Fetching all plugin settings is now done by leaving the function params empty.

--- source/custom_ffmpeg_process/plugin.py
+++ source/custom_ffmpeg_process/plugin.py
@@ -51,8 +51,8 @@ class Settings(PluginSettings):
 
     This class has a number of methods available to it for accessing these settings:
 
-        > get_setting(<key>)            - Fetch a single setting value. Or set "all" as the 
-                                        key argument and return the full dictionary.
+        > get_setting(<key>)            - Fetch a single setting value. Or leave the 
+                                        key argument empty and return the full dictionary.
         > set_setting(<key>, <value>)   - Set a singe setting value.
                                         Used by the Unmanic WebUI to save user settings.
                                         Settings are stored on disk in order to be persistent.
