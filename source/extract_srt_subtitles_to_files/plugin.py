5c926b0a78edda0c8c4e394c18579eb1aa3a4e69
Josh.5 <jsunnex@gmail.com>
2021-08-28 19:50:18 +1200

    [extract_srt_subtitles_to_files] v0.0.2

--- source/extract_srt_subtitles_to_files/plugin.py
+++ source/extract_srt_subtitles_to_files/plugin.py
@@ -19,14 +19,14 @@
         If not, see <https://www.gnu.org/licenses/>.
 
 """
+import logging
 import os
 import re
 
-from unmanic.libs.unplugins.settings import PluginSettings
+from lib.ffmpeg import Probe, Parser
 
-
-class Settings(PluginSettings):
-    settings = {}
+# Configure plugin logger
+logger = logging.getLogger("Unmanic.Plugin.extract_srt_subtitles_to_files")
 
 
 def on_worker_process(data):
@@ -34,39 +34,50 @@ def on_worker_process(data):
     Runner function - enables additional configured processing jobs during the worker stages of a task.
 
     The 'data' object argument includes:
+        exec_command            - A command that Unmanic should execute. Can be empty.
+        command_progress_parser - A function that Unmanic can use to parse the STDOUT of the command to collect progress stats. Can be empty.
+        file_in                 - The source file to be processed by the command.
+        file_out                - The destination that the command should output (may be the same as the file_in if necessary).
+        original_file_path      - The absolute path to the original file.
+        repeat                  - Boolean, should this runner be executed again once completed with the same variables.
+
+    DEPRECIATED 'data' object args passed for legacy Unmanic versions:
         exec_ffmpeg             - Boolean, should Unmanic run FFMPEG with the data returned from this plugin.
-        file_probe              - A dictionary object containing the current file probe state.
         ffmpeg_args             - A list of Unmanic's default FFMPEG args.
-        file_in                 - The source file to be processed by the FFMPEG command.
-        file_out                - The destination that the FFMPEG command will output.
-        original_file_path      - The absolute path to the original library file.
 
     :param data:
     :return:
 
     """
+
     # Default to no FFMPEG command required. This prevents the FFMPEG command from running if it is not required
-    data['exec_ffmpeg'] = False
+    data['exec_command'] = []
+    data['repeat'] = False
+    exec_ffmpeg = False
+
+    # Get the path to the file
+    abspath = data.get('file_in')
+
+    # Get file probe
+    probe = Probe(logger)
+    probe.file(abspath)
 
     # Set the file out as the file in (the source file is not modified with this plugin)
     data['file_out'] = data.get("file_in")
-
-    # Check file probe for title metadata in the video
-    file_probe = data.get('file_probe')
 
     split_original_file_path = os.path.splitext(data.get('original_file_path'))
     original_file_directory = os.path.dirname(data.get('original_file_path'))
 
     streams_to_map = []
-    probe_streams = file_probe.get('streams')
+    probe_streams = probe.get('streams')
     for probe_stream in probe_streams:
         # Check if a video stream exists with a codec name in ["mjpeg"]
         codec_type = probe_stream.get('codec_type')
         codec_name = probe_stream.get('codec_name')
         if codec_type.lower() == 'subtitle' and codec_name in ['srt', 'subrip', 'mov_text']:
             # Found a stream with an supported subtitle!
-            # Set the exec_ffmpeg param to True to run the ffmpeg command once the plugin completes
-            data['exec_ffmpeg'] = True
+            # Set the exec_ffmpeg var to True to run the ffmpeg command once the plugin completes
+            exec_ffmpeg = True
 
             # Find a tag for this subtitle
             subtitle_tag = probe_stream.get('index')
@@ -92,15 +103,30 @@ def on_worker_process(data):
                 os.path.join(original_file_directory, "{}-{}.srt".format(split_original_file_path[0], subtitle_tag)),
             ]
 
-    if data['exec_ffmpeg']:
+    # DEPRECIATED: 'exec_ffmpeg' kept for legacy Unmanic versions
+    data['exec_ffmpeg'] = False
+    if exec_ffmpeg:
         # Build ffmpeg args and add them to the return data
-        data['ffmpeg_args'] = [
+        ffmpeg_args = [
             '-i',
             data.get('file_in'),
             '-hide_banner',
             '-loglevel',
             'info',
         ]
-        data['ffmpeg_args'] += streams_to_map
+        ffmpeg_args += streams_to_map
+
+        # DEPRECIATED: 'ffmpeg_args' kept for legacy Unmanic versions
+        data['ffmpeg_args'] = ffmpeg_args
+        # DEPRECIATED: 'exec_ffmpeg' kept for legacy Unmanic versions
+        data['exec_ffmpeg'] = True
+
+        data['exec_command'] = ['ffmpeg']
+        data['exec_command'] += ffmpeg_args
+
+        # Set the parser
+        parser = Parser(logger)
+        parser.set_probe(probe)
+        data['command_progress_parser'] = parser.parse_progress
 
     return data
