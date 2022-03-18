efcdf4a7e2eb9e63bb32622dbc0bc3c018fe33f6
Josh.5 <jsunnex@gmail.com>
2022-03-19 11:53:45 +1300

    [encoder_audio_ac3] v0.0.4

--- source/encoder_audio_ac3/changelog.md
+++ source/encoder_audio_ac3/changelog.md
@@ -1,4 +1,8 @@
 
+**<span style="color:#56adda">0.0.4</span>**
+- Update FFmpeg helper
+- Add platform declaration
+
 **<span style="color:#56adda">0.0.3</span>**
 - Enabled support for v2 plugin executor
 
--- source/encoder_audio_ac3/lib/ffmpeg/probe.py
+++ source/encoder_audio_ac3/lib/ffmpeg/probe.py
@@ -27,6 +27,8 @@ import os
 import subprocess
 from logging import Logger
 
+from .mimetype_overrides import MimetypeOverrides
+
 
 class FFProbeError(Exception):
     """
@@ -107,6 +109,15 @@ class Probe(object):
             allowed_mimetypes = ['audio', 'video', 'image']
         self.allowed_mimetypes = allowed_mimetypes
 
+        # Init (reset) our mimetype list
+        mimetypes.init()
+
+        # Add mimetype overrides to mimetype dictionary (replaces any existing entries)
+        mimetype_overrides = MimetypeOverrides()
+        all_mimetype_overrides = mimetype_overrides.get_all()
+        for extension in all_mimetype_overrides:
+            mimetypes.add_type(all_mimetype_overrides.get(extension), extension)
+
     def __test_valid_mimetype(self, file_path):
         """
         Test the given file path for its mimetype.
@@ -118,7 +129,6 @@ class Probe(object):
         :return:
         """
         # Only run this check against video/audio/image MIME types
-        mimetypes.init()
         file_type = mimetypes.guess_type(file_path)[0]
 
         # If the file has no MIME type then it cannot be tested
