bd5a86923edc6f44ea0189c6e3c6c7f02fcc2557
HStep20 <Stephens.j.hunter@protonmail.com>
2022-09-07 18:12:42 -0600

    added missing files

--- source/ignore_hardlinked_files/plugin.py
+++ source/ignore_hardlinked_files/plugin.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+    unmanic-plugins.plugin.py
+
+    Written by:               Hstep20 <stephens.j.hunter@protonmail.com>
+    Date:                     09 Sep 2022
+
+    Copyright:
+        Copyright (C) 2021 Hunter Stephens
+
+        This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+        Public License as published by the Free Software Foundation, version 3.
+
+        This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+        implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+        for more details.
+
+        You should have received a copy of the GNU General Public License along with this program.
+        If not, see <https://www.gnu.org/licenses/>.
+
+"""
 import logging
 import os
 from unmanic.libs.unplugins.settings import PluginSettings
