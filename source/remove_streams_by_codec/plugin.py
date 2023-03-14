20002e673c0315ef03d4b40859b9041fd705eb88
Jefferey Neuffer <jeffereyneuffer@gmail.com>
2023-03-15 09:42:25 +1000

    [remove_streams_by_codec] add license headers

--- source/remove_streams_by_codec/plugin.py
+++ source/remove_streams_by_codec/plugin.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+    Written by:               j7126 <jeffereyneuffer@gmail.com>
+    Date:                     15 Mar 2023
+
+    Copyright:
+        Copyright (C) 2023 Jefferey Neuffer
+
+        This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+        Public License as published by the Free Software Foundation, version 3.
+
+        This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+        implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+        for more details.
+
+        You should have received a copy of the GNU General Public License along with this program.
+        If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
 import logging
 
 from unmanic.libs.unplugins.settings import PluginSettings
