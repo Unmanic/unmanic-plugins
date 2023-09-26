b61cd92fe621a65beeb9f3a5cbf51a0c7ba6bb6e
Josh.5 <jsunnex@gmail.com>
2023-09-26 18:13:40 +1300

    Add missing SPDX headers

--- source/apple_hevc/plugin.py
+++ source/apple_hevc/plugin.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+    unmanic-plugins.plugin.py
+
+    Written by:               k29t59dh <chapels.rill_0h@icloud.com>
+    Date:                     9 Aug 2023, (4:06 PM)
+
+    Copyright:
+        Copyright (C) 2021 Josh Sunnex
+
+        This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
+        Public License as published by the Free Software Foundation, version 3.
+
+        This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+        implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+        for more details.
+
+        You should have received a copy of the GNU General Public License along with this program.
+        If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
 import logging
 
 from unmanic.libs.unplugins.settings import PluginSettings
