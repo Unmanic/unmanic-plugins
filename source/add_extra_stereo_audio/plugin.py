ee124a653bff35f4858ae1de53ec06d46447301c
WillBeasley <will.beasley@outlook.com>
2023-12-07 15:41:53 +0000

    Re-add missing line that filters commentary audio streams

--- source/add_extra_stereo_audio/plugin.py
+++ source/add_extra_stereo_audio/plugin.py
@@ -74,6 +74,7 @@ def stream_to_stereo_encode(stream_language, channels, codec_name, probe_streams
     stream = audio_stream
     for  i in range(0, len(probe_streams)):
         if "codec_type" in probe_streams[i] and probe_streams[i]["codec_type"] == "audio":
+            if "tags" in probe_streams[i] and "title" in probe_streams[i]["tags"] and probe_streams[i]["tags"]["title"] == 'Commentary':
                 continue
         try:
             if "tags" in probe_streams[i] and "language" in probe_streams[i]["tags"] and probe_streams[i]["codec_name"] == "aac" and probe_streams[i]["channels"] == 2 and probe_streams[i]["channel_layout"] == "stereo" and probe_streams[i]["tags"]["language"] == stream_language: return stream
